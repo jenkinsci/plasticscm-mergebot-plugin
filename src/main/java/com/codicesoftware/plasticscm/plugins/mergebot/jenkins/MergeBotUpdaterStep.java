b63a46e7995a05b474192fc800ece96a077d73f9
Miguel González <migueljgonz@gmail.com>
2018-09-28 08:18:17 +0000

    Complete the implementation of pipeline support

--- src/main/java/com/codicesoftware/plasticscm/plugins/mergebot/jenkins/MergeBotUpdaterStep.java
+++ src/main/java/com/codicesoftware/plasticscm/plugins/mergebot/jenkins/MergeBotUpdaterStep.java
@@ -1,14 +1,33 @@
 package com.codicesoftware.plasticscm.plugins.mergebot.jenkins;
 
+import hudson.Extension;
 import hudson.scm.SCM;
 import org.jenkinsci.plugins.workflow.steps.scm.SCMStep;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 import javax.annotation.Nonnull;
 
 public class MergeBotUpdaterStep extends SCMStep {
+    @DataBoundConstructor
+    public MergeBotUpdaterStep() {
+    }
+
     @Nonnull
     @Override
     protected SCM createSCM() {
         return new MergeBotUpdater();
+    }
+
+    @Extension
+    public static final class MergeBotUpdaterStepDescriptor extends SCMStepDescriptor {
+        @Override
+        public String getFunctionName() {
+            return "mergebotCheckout";
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Plastic SCM Mergebot Checkout";
+        }
     }
 }
